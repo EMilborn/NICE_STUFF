2433a25f900091be9a3f6fe577569261839c08b5
Elias Milborn <emilborn@stuy.edu>
2016-04-13 21:19:38 -0400

    fixed FastSelect

--- FastSelect.java
+++ FastSelect.java
@@ -7,19 +7,18 @@
 public class FastSelect{
 
     public static void swap(int[] arr, int ind1, int ind2){
-	int new1 = ind2;
-	arr[ind2] = ind1;
+	int new1 = arr[ind2];
+	arr[ind2] = arr[ind1];
 	arr[ind1] = new1;
     }
 
     public static int partition(int[] arr, int pivPos, int left, int right){
-	int pivVal = arr[pivPos];
 	int savPos = left;
-
-	for (int i = left; i < right - 1; i++){
-	    if (arr[i] < pivVal){
+	swap(arr, pivPos, right);
+	for (int i = left; i < right; i++){
+	    if (arr[i] < arr[right]){
 		swap (arr, savPos, i);
-		savPos++;
+		savPos+=1;
 	    }
 	}
 
@@ -30,15 +29,15 @@ public class FastSelect{
     public static int FSel(int[] arr, int k, int lo, int hi){
 	int ret;
 	if (lo >= hi)
-	    ret =  arr[lo];
+	    ret = arr[k-1];
 	
 	else {
 	    int i = partition(arr, hi, lo, hi);
-	    if (i == k)
+	    if (i == k-1)
 		ret = arr[i];
-	    else if (i < k)
+	    else if (i > k-1)
 		ret = FSel(arr, k, lo, i-1);
-	    else 
+	    else
 		ret = FSel(arr, k, i + 1, hi);	    
 	}
 	return ret;
@@ -46,5 +45,23 @@ public class FastSelect{
     public static int FastSelect(int[] arr, int k){
 	return FSel(arr, k, 0, arr.length - 1);
     }
+
+    public static String arrToString(int[] arr){
+	String ret = "";
+	for (int i : arr)
+	    ret += i + " ";
+	return ret;
+    }
     
+    public static void main(String[] args){
+	int k = Integer.parseInt(args[0]);
+	System.out.println("k is " + k);
+	int[] arr = new int[args.length-1];
+	for (int i = 0; i < arr.length; i++){
+	    arr[i] = Integer.parseInt(args[i+1]);
+	}
+	System.out.println(arrToString(arr));
+
+	System.out.println(FastSelect(arr, k));
+    }
 }
